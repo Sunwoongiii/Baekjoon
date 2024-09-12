7c0ebd928484512473bf43ba8fd7766c42479a3c
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-09-12 19:29:46 +0900

    modified:   Baekjoon/11403_findWay.cpp 	new file:   Baekjoon/11403_findWay.exe

--- Baekjoon/11403_findWay.cpp
+++ Baekjoon/11403_findWay.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int n;
+int miro[101][101];
 
 int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+  int n;
+  cin>>n;
+  for(int i = 0; i < n; i++){
+    for(int j = 0 ; j < n; j++){
+      cin>>miro[i][j];
+    }
+  }
 
-
+  for(int k = 0; k < n; k ++){
+    for(int i = 0; i < n; i++){
+      for(int j = 0 ; j < n; j++){
+        if(miro[i][k] && miro[k][j]) miro[i][j] = 1;
+      }
+    }
+  }
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      cout<<miro[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+  return 0;
 }
