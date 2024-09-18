16dfb0c520af3f2c40138f4db473200d950bdb4e
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-09-18 13:46:28 +0900

    modified:   Baekjoon/16953_aTob.cpp

--- Baekjoon/16953_aTob.cpp
+++ Baekjoon/16953_aTob.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#define inf 1000000000+1
+
 using namespace std;
 using ll = long long;
 
-ll a,b;
-ll visited[inf];
-priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
-ll ans[inf];
+int bfs(ll a, ll b){
+  queue<pair<ll,ll>>q;
+  q.push({a,1});
+  
+  while(!q.empty()){
+    ll node = q.front().first;
+    ll value = q.front().second;
+    q.pop();
 
-ll dijkstra(){
-  pq.push({1, a});
-
-  while(!pq.empty()){
-    ll value = pq.top().first;
-    ll node = pq.top().second;
-    pq.pop();
-
-    if(visited[node]) continue;
-    visited[node] = true;
-
-    if(node == b) return ans[node];
-
-    if(node - 1 > 0 && !visited[node]){
-      pq.push({value+1, node-1});
-      visited[node-1] = true;
-    }
-
-    if(node * 2 < inf && !visited[node]){
-      pq.push({value+1, node*2});
-      visited[node*2] = true;
-    }
+    if(a > b) return -1;
+    if(node == b) return value;
+    if(node * 2 <= b) q.push({node*2, value+1});
+    if(node * 10 + 1 <= b) q.push({node*10 +1, value+1});
   }
   return -1;
 }
@@ -41,6 +26,7 @@ int main(){
   cin.tie(nullptr);
   cout.tie(nullptr);
 
+  ll a,b;
   cin>>a>>b;
-  cout<<dijkstra();
+  cout<<bfs(a,b);
 }
