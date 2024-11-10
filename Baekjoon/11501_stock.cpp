136d16febb2594c5e3b49a687e42acb5bcbdd2a8
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-11-10 16:30:55 +0900

    modified:   Baekjoon/11501_stock.cpp 	new file:   Baekjoon/11501_stock.exe

--- Baekjoon/11501_stock.cpp
+++ Baekjoon/11501_stock.cpp
@@ -3,5 +3,31 @@
 #include <algorithm>
 using namespace std;
 
-int t;
-vector<int> 
+int T,n;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  cin>>T;
+  while(T--){
+    cin>>n;
+    vector<int>v;
+
+    for(int i = 0; i < n; i++){
+      int x;
+      cin>>x;
+      v.push_back(x);
+    }
+
+    long long result = 0;
+    int maxi = -1;
+
+    for(int i = n-1; i >= 0; i--){
+      maxi = max(maxi, v[i]);
+      result += maxi - v[i];
+    }
+    cout<<result<<'\n';
+  }
+}
