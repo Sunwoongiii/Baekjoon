c68170a8dbe3a28d02b679dcdeee8a11c4e6838f
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-11-11 13:14:15 +0900

    new file:   Baekjoon/30804_Tang.cpp 	modified:   Baekjoon/5525_IOIOI.cpp 	new file:   Baekjoon/5525_IOIOI.exe

--- Baekjoon/5525_IOIOI.cpp
+++ Baekjoon/5525_IOIOI.cpp
@@ -2,17 +2,28 @@
 #include <string>
 using namespace std;
 
-int n,m;
-string s;
-
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  cin>>n>>m>>s;
+  int n,m;
+  string str;
+  cin>>n>>m>>str;
+
+  int ans = 0;
 
   for(int i = 0; i < m; i++){
-    
+    int k = 0;
+    if(str[i] == 'O')continue;
+    while(str[i+1] == 'O' && str[i+2] == 'I'){
+      k++;
+      if(k == n){
+        ans++;
+        k--;
+      }
+      i+=2;
+    }
   }
+  cout<<ans;
 }
