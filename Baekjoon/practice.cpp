369725ade5b7a87dfc5a7570b96de1db7908731d
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-12-02 18:07:21 +0900

    modified:   Baekjoon/practice.cpp 	modified:   Baekjoon/practice.exe

--- Baekjoon/practice.cpp
+++ Baekjoon/practice.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <tuple>
 using namespace std;
 
-class Circle{
-  int radius;
+int N,K,H;
+priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+vector<pair<int,int>>v[200200];
+int ans[200200];
 
-public:
-  Circle(int radius = 0){this->radius = radius;}
-  void show()cout<<"Circle's radius = "<<radius<<endl;
-
-  friend
+void getInput(){
+  cin>>N>>K>>H;
+  for(int i =0; i < K; K++){
+    int a,b,c;
+    v[a].push_back({c,b});
+    v[b].push_back({c,a});
+  }
 }
 
-int main(){
-  Circle a(5),b;
-  b = ++a;
-  a.show();
-  b.show();
-  b = a++;
-  a.show();
-  b.show();
+int dijkstra(int start){
+  fill(ans, ans+N+1, 1e9);
+  pq.push({0,start});
+  ans[start] = 0;
+
+  while(!pq.empty()){
+    int value, node, HP;
+    tie(value, node, HP) = pq.top();
+    pq.pop();
+
+    if(HP == 0)return value;
+
+    for(int i = 0; i < v[node].size(); i++){
+      int cost = v[node][i].first;
+      int next = v[node][i].second;
+
+      if(ans[next] > ans[node] + cost){
+        ans[next] = ans[node] + cost;
+        pq.push({ans[next], next, HP--})
+      }
+    }
+  }
+  return -1;
 }
