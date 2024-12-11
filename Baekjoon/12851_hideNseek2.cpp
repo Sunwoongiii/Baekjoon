512e613944402acd698f9366ce02d2b5feba3975
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-12-11 10:47:16 +0900

    modified:   Baekjoon/12851_hideNseek2.cpp 	modified:   Baekjoon/12851_hideNseek2.exe 	modified:   Baekjoon/practice.cpp 	deleted:    Baekjoon/practice.exe

--- Baekjoon/12851_hideNseek2.cpp
+++ Baekjoon/12851_hideNseek2.cpp
@@ -1,50 +1,44 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
-#include <vector>
-#define inf 100001
-
 using namespace std;
 
-int n,k;
-bool visited[inf];
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-
-int dijkstra(){
-  pq.push({0,n});
-  while(!pq.empty()){
-    int value = pq.top().first;
-    int node = pq.top().second;
-    pq.pop();
-
-    if(node == k) return value;
-
-    if(visited[node])continue;
-    visited[node] = true;
-
-    if(node * 2 < inf){
-      pq.push({value+1, node*2});
-      visited[node] = true;
-    }
-
-    if(node +1 < inf){
-      pq.push({value+1, node+1});
-      visited[node] = true;
-    }
-
-    if(node -1 >= 0){
-      pq.push({value+1, node-1});
-      visited[node] = true;
-    }
-  }
-}
+int dist[100100];
+int cnt[100100];
+int n, k;
+queue<int> q;
 
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
-
-  cin>>n>>k;
-  cout<<dijkstra()<<"\n";
   
-  return 0;
+  cin>>n>>k;
+  if(n == k){
+    cout<<0<<'\n'<<1;
+    return 0;
+  }
+
+  fill(dist, dist+100100, -1);
+  dist[n] = 0;
+  cnt[n] = 1;
+  q.push(n);
+
+  while(!q.empty()){
+    int node = q.front();
+    q.pop();
+
+    for(int next : {node-1, node+1, node*2}){
+      if(next < 0 || next > 100000) continue;
+      if(dist[next] != -1){
+        if(dist[node] + 1 == dist[next]) cnt[next] += cnt[node];
+      }
+      else{
+        dist[next] = dist[node] + 1;
+        cnt[next] = cnt[node];
+        q.push(next);
+      }
+    }
+  }
+  cout<<dist[k]<<'\n'<<cnt[k];
 }
